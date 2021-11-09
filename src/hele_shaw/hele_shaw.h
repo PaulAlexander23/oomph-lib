216b4c3a8e26b201647dcf07e7f737a72e685b5c
PaulAlexander23 <PaulAlexander23@users.noreply.github.com>
2021-11-09 18:26:07 +0000

    Automated commit of clang-format CI changes.

---
+++ src/hele_shaw/hele_shaw.h
@@ -0,0 +1,11 @@
+// This file was generated automatically during the make process
+// and it will be remade automatically
+#include <hele_shaw/hele_shaw_elements.h>
+#include <hele_shaw/hele_shaw_equations.h>
+#include <hele_shaw/hele_shaw_flux_elements.h>
+#include <hele_shaw/Thele_shaw_elements.h>
+#include <hele_shaw/hele_shaw_elements.template.cc>
+#include <hele_shaw/projectable_hele_shaw_element.h>
+#include <hele_shaw/projectable_hele_shaw_element.template.cc>
+#include <hele_shaw/hele_shaw_interface_elements.h>
+#include <hele_shaw/hele_shaw_interface_elements_with_integrals.h>
