8b344fd46838cac002dad70b60f64c86ec75602b
Paul Alexander <paulalexander23@live.co.uk>
2021-11-23 14:40:42 +0000

    Fill out example with fluid and bubble
    Currently unable to solve the unsteady newton problem

--- demo_drivers/hele_shaw/relaxing_bubble/relaxing_bubble.cc
+++ demo_drivers/hele_shaw/relaxing_bubble/relaxing_bubble.cc
@@ -3,8 +3,12 @@
 #include "generic.h"
 //#include "meshes.h"
 #include "hele_shaw.h"
+#include "solid.h"
+#include "constitutive.h"
+#include "fluid_interface.h"
 
 #include "relaxing_bubble_problem.h"
+#include "custom_hele_shaw_elements_with_integrals.h"
 
 using namespace oomph;
 using namespace std;
@@ -20,22 +24,28 @@ int main(int argc, char* argv[])
   DocInfo doc_info;
 
   /// Output directory
-  doc_info.set_directory("RESLT");
+  doc_info.set_directory("RESLT/");
 
   /// Set parameters
   relaxing_bubble::q_inv_pt = new double;
   relaxing_bubble::st_pt = new double;
   relaxing_bubble::alpha_pt = new double;
-  *relaxing_bubble::q_inv_pt = 0.01;
+  relaxing_bubble::nu_pt = new double;
+  *relaxing_bubble::q_inv_pt = 20;
   *relaxing_bubble::st_pt = 1;
-  *relaxing_bubble::alpha_pt = 0.1;
+  *relaxing_bubble::alpha_pt = 10;
+  *relaxing_bubble::nu_pt = 0.3;
+  // Create generalised Hookean constitutive equations
+  relaxing_bubble::constitutive_law_pt =
+    new GeneralisedHookean(relaxing_bubble::nu_pt);
 
   /// Create problem
   RelaxingBubbleProblem<ProjectableHeleShawElement<
     PseudoSolidNodeUpdateElement<THeleShawElement<3>, TPVDElement<2, 3>>>>
     problem;
+  //RelaxingBubbleProblem<MyNewElement> problem;
 
-  bool run_self_test = false;
+  bool run_self_test = true;
   if (run_self_test)
   {
     if (problem.self_test())
@@ -48,11 +58,17 @@ int main(int argc, char* argv[])
   /// Solve for initial conditions
   problem.solve_for_initial_conditions(doc_info);
 
-  double dt = 0.1;
-  double tF = 0.5;
+  double dt = 1e-3;
+  double tF = 1e-3;
 
   /// Iterate the timestepper using the fixed time step until the final time
   problem.iterate_timestepper(dt, tF, doc_info);
 
+  delete relaxing_bubble::q_inv_pt;
+  delete relaxing_bubble::st_pt;
+  delete relaxing_bubble::alpha_pt;
+  delete relaxing_bubble::nu_pt;
+  delete relaxing_bubble::constitutive_law_pt;
+  
   return 0;
 }
