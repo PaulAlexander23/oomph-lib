9efe8e02e11635f14c4e7eb68dcfc51036f0f3c1
Paul Alexander <paulalexander23@live.co.uk>
2021-11-24 15:33:45 +0000

    Make changes to the steady demos

--- demo_drivers/hele_shaw/steady/validate.sh
+++ demo_drivers/hele_shaw/steady/validate.sh
@@ -5,7 +5,7 @@ OOMPH_ROOT_DIR=$(make -s --no-print-directory print-top_builddir)
 
 
 #Set the number of tests to be checked
-NUM_TESTS=3
+NUM_TESTS=5
 
 
 # Setup validation directory
@@ -82,6 +82,50 @@ if test "$1" = "no_fpdiff"; then
 else
 ../../../../bin/fpdiff.py ../validata/hele_shaw_perturbed_channel_flow_quad_results.dat.gz   \
     hele_shaw_perturbed_channel_flow_quad_results.dat 0.1 1e-13  >> validation.log
+fi
+
+echo "Running Hele-Shaw taped channel flow quad validation "
+mkdir RESLT
+../hele_shaw_taped_channel_flow_quad > OUTPUT_hele_shaw_taped_channel_flow_quad
+echo "done"
+echo " " >> validation.log
+echo "Hele-Shaw taped channel flow quad validation" >> validation.log
+echo "--------------------------" >> validation.log
+echo " " >> validation.log
+echo "Validation directory: " >> validation.log
+echo " " >> validation.log
+echo "  " `pwd` >> validation.log
+echo " " >> validation.log
+cat RESLT/* > hele_shaw_taped_channel_flow_quad_results.dat
+rm RESLT -r -f
+
+if test "$1" = "no_fpdiff"; then
+  echo "dummy [OK] -- Can't run fpdiff.py because we don't have python or validata" >> validation.log
+else
+../../../../bin/fpdiff.py ../validata/hele_shaw_taped_channel_flow_quad_results.dat.gz   \
+    hele_shaw_taped_channel_flow_quad_results.dat 0.1 1e-13  >> validation.log
+fi
+
+echo "Running Hele-Shaw taped channel flow quad with integral validation "
+mkdir RESLT
+../hele_shaw_taped_channel_flow_quad_with_integral > OUTPUT_hele_shaw_taped_channel_flow_quad_with_integral
+echo "done"
+echo " " >> validation.log
+echo "Hele-Shaw taped channel flow quad with integral validation" >> validation.log
+echo "--------------------------" >> validation.log
+echo " " >> validation.log
+echo "Validation directory: " >> validation.log
+echo " " >> validation.log
+echo "  " `pwd` >> validation.log
+echo " " >> validation.log
+cat RESLT/* > hele_shaw_taped_channel_flow_quad_with_integral_results.dat
+rm RESLT -r -f
+
+if test "$1" = "no_fpdiff"; then
+  echo "dummy [OK] -- Can't run fpdiff.py because we don't have python or validata" >> validation.log
+else
+../../../../bin/fpdiff.py ../validata/hele_shaw_taped_channel_flow_quad_with_integral_results.dat.gz   \
+    hele_shaw_taped_channel_flow_quad_with_integral_results.dat 0.1 1e-11  >> validation.log
 fi
 
 # Append output to global validation log file
