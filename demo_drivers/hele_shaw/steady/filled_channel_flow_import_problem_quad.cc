9919b0d7a948a8352d9b9a858edd00c4ae54fdd8
Paul Alexander <paulalexander23@live.co.uk>
2021-11-24 15:33:45 +0000

    Make changes to the steady demos

---
+++ demo_drivers/hele_shaw/steady/filled_channel_flow_import_problem_quad.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+
+#include "generic.h"
+#include "meshes.h"
+#include "hele_shaw.h"
+#include "info_element.h"
+
+#include "filled_channel_flow_problem.cc"
+
+using namespace oomph;
+using namespace std;
+
+template<class ELEMENT>
+class QHeleShawChannelProblem : public HeleShawChannelProblem<ELEMENT>
+{
+public:
+  QHeleShawChannelProblem() : HeleShawChannelProblem<ELEMENT>(){};
+
+  ~QHeleShawChannelProblem(){};
+
+  void generate_bulk_mesh();
+};
+
+template<class ELEMENT>
+void QHeleShawChannelProblem<ELEMENT>::generate_bulk_mesh()
+{
+  unsigned n_x = 10;
+  unsigned n_y = 10;
+  double l_x = 2.0;
+  double l_y = 1.0;
+
+  cout << "SimpleRectangularQuadMesh" << endl;
+  this->Bulk_mesh_pt =
+    new SimpleRectangularQuadMesh<ELEMENT>(n_x, n_y, l_x, l_y);
+}
+
+int main(int argc, char* argv[])
+{
+  /// Store command line arguments
+  CommandLineArgs::setup(argc, argv);
+
+  /// Create a DocInfo object for output processing
+  DocInfo doc_info;
+
+  /// Output directory
+  doc_info.set_directory("RESLT");
+
+  /// Create problem object
+  QHeleShawChannelProblem<QHeleShawElement<3>> problem;
+
+  /// Run problem self test
+  if (problem.self_test())
+  {
+    throw OomphLibError(
+      "Self test failed", OOMPH_CURRENT_FUNCTION, OOMPH_EXCEPTION_LOCATION);
+  }
+
+  /// Call problem solve
+  problem.newton_solve();
+
+  /// Document solution
+  problem.doc_solution(doc_info);
+
+  return 0;
+}
