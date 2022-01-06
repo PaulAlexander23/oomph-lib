a99bf2520c91cd6bd948dda567679ee0d32365a4
Paul Alexander <paulalexander23@live.co.uk>
2022-01-06 17:35:52 +0000

    Add a inject bubble demo

---
+++ demo_drivers/hele_shaw/inject_bubble/inject_air_parameters.h
@@ -0,0 +1,52 @@
+#ifndef OOMPH_INJECT_AIR_PARAMETERS_HEADER
+#define OOMPH_INJECT_AIR_PARAMETERS_HEADER
+
+#include "generic.h"
+
+namespace parameters
+{
+  double* q_inv_pt = 0;
+  double* st_pt = 0;
+  double* alpha_pt = 0;
+  /// Pseudo-solid Poisson ratio
+  double* nu_pt = 0;
+  double* bubble_pressure_pt = 0;
+  double* target_bubble_volume_pt = 0;
+  double* target_fluid_volume_pt = 0;
+  double* total_volume_pt = 0;
+
+  const double initial_volume = MathematicalConstants::Pi * pow(0.3, 2.0);
+  const double injection_rate = 1.0;
+
+  ConstitutiveLaw* constitutive_law_pt = 0;
+
+  void channel_depth(const Vector<double>& x, double& b)
+  {
+    b = 1.0;
+  }
+
+  void upper_wall_fct(const Vector<double>& x, double& b, double& dbdt)
+  {
+    channel_depth(x, b);
+    dbdt = 0.0;
+  }
+
+  void wall_speed_fct(const Vector<double>& x, Vector<double>& U_frame)
+  {
+    U_frame[0] = 0.0;
+    U_frame[1] = 0.0;
+  }
+
+  void bubble_pressure_fct(const Vector<double>& x, double& pressure)
+  {
+    pressure = (*bubble_pressure_pt);
+  }
+
+  void volume_fct(const double& t, double& volume)
+  {
+    volume = initial_volume + injection_rate * t;
+  }
+
+} // namespace parameters
+
+#endif
