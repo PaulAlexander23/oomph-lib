24ea95d4998f6c39de1d80dcac1a894e7c2a8b41
Paul Alexander <paulalexander23@live.co.uk>
2022-01-12 14:38:02 +0000

    Correct parameter converter and add a self test

--- src/hele_shaw/hele_shaw_parameter_converter.h
+++ src/hele_shaw/hele_shaw_parameter_converter.h
@@ -3,35 +3,71 @@
 
 namespace oomph
 {
-  void convert_to_capillary_nondimensionalisation(const double& r,
-                                                  const double& width,
-                                                  const double& volume,
-                                                  const double& Q,
-                                                  const double& aspect_ratio,
-                                                  double& length_ratio,
-                                                  double& pressure_ratio,
-                                                  double& velocity_ratio,
-                                                  double& time_ratio,
-                                                  double& new_r,
-                                                  double& new_width,
-                                                  double& new_volume,
-                                                  double& new_Ca)
+  /// Get the ratios of the different independent variables scaled first by
+  /// fixing the pressure gradient at the end of the domain to 1, to the
+  /// second method of fixing the flux at the end of the channel to 1.
+  void get_ratios_of_q_nd_to_capillary_nd(double& length_ratio,
+                                          double& depth_ratio,
+                                          double& time_ratio,
+                                          double& pressure_ratio)
   {
-    // const double G = 1.9648098177825899; //20
-    // const double G = 1.9648144969660464; //40
-    const double G = 1.9648144083521897; // 80
-
+    /// I = int_{-1}^{1} (\bar{b}(y))^3 dy
+    /// where b =
+    /// Calculated using a simple finite element code.
+    /// Using 2 elements
+    // const double I = 1.9648098177825899;
+    /// Using 40 elements
+    // const double I = 1.9648144969660464;
+    /// Using 80 elements, accurate to at least 6dp
+    const double I = 1.9648144083521897;
 
     /// Ratios of old to new
     length_ratio = 0.5;
-    pressure_ratio = 12;
-    time_ratio = 1;
+    depth_ratio = 1.0;
+    time_ratio = I / 4.0;
+    pressure_ratio = 12.0 / I;
+  }
 
-    /// New parameters
-    new_r = r * length_ratio;
-    new_width = width * length_ratio;
-    new_volume = volume * length_ratio * length_ratio;
-    new_Ca = Q * aspect_ratio / 2;
+  /// Convert the parameters used from a nondimensionalation which
+  /// fixes the pressure gradient at the end of the domain to 1, to a
+  /// second method which fixes the flux at the end of the channel to 1.
+  void convert_parameters_from_q_nd_to_capillary_nd(const double& Q,
+                                                    const double& V,
+                                                    const double& h,
+                                                    double& Ca,
+                                                    double& new_V,
+                                                    double& new_h)
+  {
+    double length_ratio;
+    double depth_ratio;
+    double time_ratio;
+    double pressure_ratio;
+    get_ratios_of_q_nd_to_capillary_nd(
+      length_ratio, depth_ratio, time_ratio, pressure_ratio);
+    Ca = 6.0 / pressure_ratio * Q;
+    new_V = length_ratio * length_ratio * depth_ratio * V;
+    new_h = depth_ratio * h;
+  }
+
+  /// Convert the parameters used from a nondimensionalation which
+  /// fixes the pressure gradient at the end of the domain to 1, to a
+  /// second method which fixes the flux at the end of the channel to 1.
+  void convert_parameters_from_capillary_nd_to_q_nd(const double& Ca,
+                                                    const double& V,
+                                                    const double& h,
+                                                    double& Q,
+                                                    double& new_V,
+                                                    double& new_h)
+  {
+    double length_ratio;
+    double depth_ratio;
+    double time_ratio;
+    double pressure_ratio;
+    get_ratios_of_q_nd_to_capillary_nd(
+      length_ratio, depth_ratio, time_ratio, pressure_ratio);
+    Q = pressure_ratio / 6.0 * Ca;
+    new_V = V / (length_ratio * length_ratio * depth_ratio);
+    new_h = h / depth_ratio;
   }
 
 } // namespace oomph
