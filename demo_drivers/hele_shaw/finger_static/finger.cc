51d601394e69dde844e5aadbf08503af0ea290c5
Paul Alexander <paulalexander23@live.co.uk>
2022-01-17 16:07:35 +0000

    Add a finger demo but with a static interface
    For using in debugging the finger demo.

---
+++ demo_drivers/hele_shaw/finger_static/finger.cc
@@ -0,0 +1,116 @@
+#include <iostream>
+
+#include "generic.h"
+//#include "meshes.h"
+#include "hele_shaw.h"
+#include "solid.h"
+#include "constitutive.h"
+#include "fluid_interface.h"
+
+#include "finger_problem.h"
+#include "integral_problem.h"
+
+using namespace oomph;
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  cout << "Finger demo" << endl;
+
+  /// Store command line arguments
+  CommandLineArgs::setup(argc, argv);
+
+  /// Create a DocInfo object for output processing
+  DocInfo doc_info;
+
+  /// Output directory
+  doc_info.set_directory("RESLT/");
+
+  /// Set parameters
+  const double major_radius = 0.5;
+  const double width = 0.25;
+  const double circular_radius = 0.0;
+  const double volume =
+    MathematicalConstants::Pi * circular_radius * circular_radius;
+  const double Q = 0.05;
+
+  double length_ratio;
+  double pressure_ratio;
+  double velocity_ratio;
+  double time_ratio;
+  double new_r;
+  double new_width;
+  double new_volume;
+  double new_Ca;
+
+  finger::alpha = 40.0;
+
+  convert_to_capillary_nondimensionalisation(major_radius,
+                                             width,
+                                             volume,
+                                             Q,
+                                             finger::alpha,
+                                             length_ratio,
+                                             pressure_ratio,
+                                             velocity_ratio,
+                                             time_ratio,
+                                             new_r,
+                                             new_width,
+                                             new_volume,
+                                             new_Ca);
+  finger::major_radius = new_r;
+  finger::ca_inv = 1.0 / new_Ca;
+  finger::st = 1.0;
+  finger::nu = 0.3;
+
+  finger::bubble_initial_centre_y = 0.5 + 0.005;
+
+  finger::perturbation_amplitude = 0.0;
+  finger::perturbation_rms_width = new_width;
+
+  // finger::target_bubble_volume = 0.01; // new_volume;
+  // Create generalised Hookean constitutive equations
+  finger::constitutive_law_pt = new GeneralisedHookean(&finger::nu);
+
+  // IntegralProblem<QIntegralElement<3>>
+  // integral_problem(finger::channel_depth); integral_problem.newton_solve();
+  // integral_problem.doc_solution(doc_info);
+  // finger::total_volume = integral_problem.result();
+
+  finger::target_bubble_volume = MathematicalConstants::Pi * pow(finger::finger_width, 2.0) / 2;
+  finger::total_volume = 1.0;
+
+  finger::target_fluid_volume =
+    (finger::total_volume) - (finger::target_bubble_volume);
+
+  finger::total_flux = 1.0;
+
+  /// Print the parameters
+  finger::print_parameters();
+
+  /// Create problem
+  FingerProblem<MyNewElementWithIntegral> problem;
+
+  bool run_self_test = false;
+  if (run_self_test)
+  {
+    if (problem.self_test())
+    {
+      throw OomphLibError(
+        "Self test failed", OOMPH_CURRENT_FUNCTION, OOMPH_EXCEPTION_LOCATION);
+    }
+  }
+
+  /// Solve for initial conditions
+  problem.solve_for_initial_conditions(doc_info);
+
+  double dt = 1e-4;
+  double tF = 1e-2;
+
+  /// Iterate the timestepper using the fixed time step until the final time
+  problem.iterate_timestepper(dt, tF, doc_info);
+
+  delete finger::constitutive_law_pt;
+
+  return 0;
+}
---
+++ demo_drivers/hele_shaw/finger_static/info_element.h
@@ -0,0 +1,81 @@
+#ifndef DATA_ELEMENT_HEADER
+#define DATA_ELEMENT_HEADER
+
+#include "generic.h"
+
+namespace oomph
+{
+  class InfoElement : public virtual GeneralisedElement
+  {
+  public:
+    InfoElement();
+
+    InfoElement(Data* const& data_pt);
+
+    ~InfoElement() {}
+
+    void add_data_pt(Data* const& data_pt);
+
+    /// Add the element's contribution to its residual vector
+    inline void fill_in_contribution_to_residuals(Vector<double>& residuals)
+    {
+      // Call the generic residuals function with flag set to 0
+      // using a dummy matrix argument
+      bool compute_jacobian = false;
+      fill_in_generic_residual_contribution(
+        residuals, GeneralisedElement::Dummy_matrix, compute_jacobian);
+    }
+
+  private:
+    void fill_in_generic_residual_contribution(Vector<double>& residuals,
+                                               DenseMatrix<double>& jacobian,
+                                               const unsigned& flag);
+  };
+
+  InfoElement::InfoElement() : GeneralisedElement() {}
+
+  InfoElement::InfoElement(Data* const& data_pt) : GeneralisedElement()
+  {
+    this->add_data_pt(data_pt);
+  }
+
+  void InfoElement::add_data_pt(Data* const& data_pt)
+  {
+    bool use_fd_for_jacobian = false;
+    this->add_internal_data(data_pt, use_fd_for_jacobian);
+  }
+
+  void InfoElement::fill_in_generic_residual_contribution(
+    Vector<double>& residuals,
+    DenseMatrix<double>& jacobian,
+    const unsigned& flag)
+  {
+    unsigned n_data = this->ninternal_data();
+    for (unsigned i_data = 0; i_data < n_data; i_data++)
+    {
+      unsigned n_value = this->internal_data_pt(i_data)->nvalue();
+      for (unsigned i_value = 0; i_value < n_value; i_value++)
+      {
+        int local_eqn = this->internal_local_eqn(i_data, i_value);
+        if (local_eqn >= 0)
+        {
+          /// The contribution to the data is minus the value as it is moved
+          /// from the right hand side of the equation
+          residuals[local_eqn] +=
+            -this->internal_data_pt(i_data)->value(i_value);
+        }
+
+        if (flag)
+        {
+          int external_unknown = this->external_local_eqn(i_data, i_value);
+          if (external_unknown >= 0)
+          {
+            jacobian(local_eqn, external_unknown) += 1.0;
+            cout << "hello!" << endl;
+          }
+        }
+      }
+    }
+  }
+} // namespace oomph
+#endif
