51f69226d47764a06d49665a00d5a72b23f4c734
Paul Alexander <paulalexander23@live.co.uk>
2022-02-15 17:47:39 +0000

    Remove .template.cc files.

--- src/hele_shaw/hele_shaw_elements.h
+++ src/hele_shaw/hele_shaw_elements.h
@@ -149,8 +149,106 @@ namespace oomph
       DenseMatrix<double>& djacobian_dX) const;
   };
 
+  // Inline functions:
+
+  //======================================================================
+  /// Define the shape functions and test functions and derivatives
+  /// w.r.t. global coordinates and return Jacobian of mapping.
+  ///
+  /// Galerkin: Test functions = shape functions
+  //======================================================================
+  template<unsigned NNODE_1D>
+  double QHeleShawElement<NNODE_1D>::dshape_and_dtest_eulerian_hele_shaw(
+    const Vector<double>& s,
+    Shape& psi,
+    DShape& dpsidx,
+    Shape& test,
+    DShape& dtestdx) const
+  {
+    // Call the geometrical shape functions and derivatives
+    const double J = this->dshape_eulerian(s, psi, dpsidx);
+
+    // Set the test functions equal to the shape functions
+    test = psi;
+    dtestdx = dpsidx;
+
+    // Return the jacobian
+    return J;
+  }
+
+  //======================================================================
+  /// Define the shape functions and test functions and derivatives
+  /// w.r.t. global coordinates and return Jacobian of mapping.
+  ///
+  /// Galerkin: Test functions = shape functions
+  //======================================================================
+  template<unsigned NNODE_1D>
+  double QHeleShawElement<NNODE_1D>::
+    dshape_and_dtest_eulerian_at_knot_hele_shaw(const unsigned& ipt,
+                                                Shape& psi,
+                                                DShape& dpsidx,
+                                                Shape& test,
+                                                DShape& dtestdx) const
+  {
+    // Call the geometrical shape functions and derivatives
+    const double J = this->dshape_eulerian_at_knot(ipt, psi, dpsidx);
+
+    // Set the pointers of the test functions
+    test = psi;
+    dtestdx = dpsidx;
+
+    // Return the jacobian
+    return J;
+  }
+
+  /// Define the shape functions (psi) and test functions (test) and
+  /// their derivatives w.r.t. global coordinates (dpsidx and dtestdx)
+  /// and return Jacobian of mapping (J). Additionally compute the
+  /// derivatives of dpsidx, dtestdx and J w.r.t. nodal coordinates.
+  ///
+  /// Galerkin: Test functions = shape functions
+  //======================================================================
+  template<unsigned NNODE_1D>
+  double QHeleShawElement<NNODE_1D>::
+    dshape_and_dtest_eulerian_at_knot_hele_shaw(
+      const unsigned& ipt,
+      Shape& psi,
+      DShape& dpsidx,
+      RankFourTensor<double>& d_dpsidx_dX,
+      Shape& test,
+      DShape& dtestdx,
+      RankFourTensor<double>& d_dtestdx_dX,
+      DenseMatrix<double>& djacobian_dX) const
+  {
+    // Call the geometrical shape functions and derivatives
+    const double J = this->dshape_eulerian_at_knot(
+      ipt, psi, dpsidx, djacobian_dX, d_dpsidx_dX);
+
+    // Set the pointers of the test functions
+    test = psi;
+    dtestdx = dpsidx;
+    d_dtestdx_dX = d_dpsidx_dX;
+
+    // Return the jacobian
+    return J;
+  }
+
+  //=======================================================================
+  /// Face geometry for the QHeleShawElement elements: The spatial
+  /// dimension of the face elements is one lower than that of the
+  /// bulk element but they have the same number of points
+  /// along their 1D edges.
+  //=======================================================================
+  template<unsigned NNODE_1D>
+  class FaceGeometry<QHeleShawElement<NNODE_1D>>
+    : public virtual QElement<1, NNODE_1D>
+  {
+  public:
+    /// \short Constructor: Call the constructor for the
+    /// appropriate lower-dimensional QElement
+    FaceGeometry() : QElement<1, NNODE_1D>() {}
+  };
+
+
 } // namespace oomph
-
-#include "hele_shaw_elements.template.cc"
-
 #endif
