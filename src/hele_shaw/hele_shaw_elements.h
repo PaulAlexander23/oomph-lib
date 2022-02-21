d8ef68e8ea04a5d0a91bf22e4f0356ac1fde90e2
Paul Alexander <paulalexander23@live.co.uk>
2022-02-21 13:53:22 +0000

    Remove implementation scripts and use headers only
    Update git ignore to include hele_shaw.h

--- src/hele_shaw/hele_shaw_elements.h
+++ src/hele_shaw/hele_shaw_elements.h
@@ -5,7 +5,7 @@
 // LIC//
 // LIC//           Version 0.90. August 3, 2009.
 // LIC//
-// LIC// Copyright (C) 2006-2009 Matthias Heil and Andrew Hazel
+// LIC// Copyright (C) 2006-2022 Matthias Heil and Andrew Hazel
 // LIC//
 // LIC// This library is free software; you can redistribute it and/or
 // LIC// modify it under the terms of the GNU Lesser General Public
@@ -35,12 +35,6 @@
 #endif
 
 // OOMPH-LIB headers
-
-// hierher uncomment these
-//#include "../generic/nodes.h"
-//#include "../generic/Qelements.h"
-//#include "../generic/oomph_utilities.h"
-
 #include "../generic/Qelements.h"
 #include "../generic/nodes.h"
 #include "../generic/oomph_utilities.h"
@@ -53,7 +47,6 @@ namespace oomph
   /// QHeleShawElement elements are linear/quadrilateral/brick-shaped
   /// HeleShaw elements with isoparametric interpolation for the function.
   //======================================================================
-  // JACK - SPECIFIC ELEMENT
   template<unsigned NNODE_1D>
   class QHeleShawElement : public virtual QElement<2, NNODE_1D>,
                            public virtual HeleShawEquations
@@ -249,6 +242,91 @@ namespace oomph
     FaceGeometry() : QElement<1, NNODE_1D>() {}
   };
 
+  /// ----------------------------------------
+  /// ----------------------------------------
+  /// ----------------------------------------
+  ///#include "hele_shaw_elements.h"
+
+  //======================================================================
+  /// QHeleShawElement elements are linear/quadrilateral/brick-shaped
+  /// HeleShaw elements with isoparametric interpolation for the function.
+  //======================================================================
+
+  /// \short Output function:
+  ///  x,y,u   or    x,y,z,u
+  template<unsigned NNODE_1D>
+  void QHeleShawElement<NNODE_1D>::output(std::ostream& outfile)
+  {
+    HeleShawEquations::output(outfile);
+  }
+
+  ///  \short Output function:
+  ///   x,y,u   or    x,y,z,u at n_plot^2 plot points
+  template<unsigned NNODE_1D>
+  void QHeleShawElement<NNODE_1D>::output(std::ostream& outfile,
+                                          const unsigned& n_plot)
+  {
+    HeleShawEquations::output(outfile, n_plot);
+  }
+
+  /// \short C-style output function:
+  ///  x,y,u   or    x,y,z,u
+  template<unsigned NNODE_1D>
+  void QHeleShawElement<NNODE_1D>::output(FILE* file_pt)
+  {
+    HeleShawEquations::output(file_pt);
+  }
+
+  ///  \short C-style output function:
+  ///   x,y,u   or    x,y,z,u at n_plot^2 plot points
+  template<unsigned NNODE_1D>
+  void QHeleShawElement<NNODE_1D>::output(FILE* file_pt, const unsigned& n_plot)
+  {
+    HeleShawEquations::output(file_pt, n_plot);
+  }
+
+  /// \short Output function for an exact solution:
+  ///  x,y,u_exact   or    x,y,z,u_exact at n_plot^2 plot points
+  template<unsigned NNODE_1D>
+  void QHeleShawElement<NNODE_1D>::output_fct(
+    std::ostream& outfile,
+    const unsigned& n_plot,
+    FiniteElement::SteadyExactSolutionFctPt exact_soln_pt)
+  {
+    HeleShawEquations::output_fct(outfile, n_plot, exact_soln_pt);
+  }
+
+  /// \short Output function for a time-dependent exact solution.
+  ///  x,y,u_exact   or    x,y,z,u_exact at n_plot^2 plot points
+  /// (Calls the steady version)
+  template<unsigned NNODE_1D>
+  void QHeleShawElement<NNODE_1D>::output_fct(
+    std::ostream& outfile,
+    const unsigned& n_plot,
+    const double& time,
+    FiniteElement::UnsteadyExactSolutionFctPt exact_soln_pt)
+  {
+    HeleShawEquations::output_fct(outfile, n_plot, time, exact_soln_pt);
+  }
+
+  ////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////
+
+  //======================================================================
+  /// Set the data for the number of Variables at each node, always one
+  /// in every case
+  //======================================================================
+  template<unsigned NNODE_1D>
+  const unsigned QHeleShawElement<NNODE_1D>::Initial_Nvalue = 1;
+
+
+  //====================================================================
+  /// Force build of templates
+  //====================================================================
+  //template class QHeleShawElement<2>;
+  //template class QHeleShawElement<3>;
+  //template class QHeleShawElement<4>;
 
 } // namespace oomph
 #endif
