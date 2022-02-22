756d0922f1479f993eb1eae817a392e679a347bf
Paul Alexander <paulalexander23@live.co.uk>
2022-02-22 09:37:38 +0000

    Remove integral elements and demos.

--- src/integral/integral.h
+++
@@ -1,4 +0,0 @@
-// This file was generated automatically during the make process
-// and it will be remade automatically
-#include <integral/integral_elements.h>
-#include <integral/Tintegral_elements.h>
