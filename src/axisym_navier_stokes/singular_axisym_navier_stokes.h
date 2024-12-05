beb1b51bc3edd3991d2d5afa4babc531810b451e
Paul Alexander <paulalexander23@live.co.uk>
2024-12-05 16:38:25 +0000

    Add common headers to NS folders.|

---
+++ src/axisym_navier_stokes/singular_axisym_navier_stokes.h
@@ -0,0 +1,6 @@
+// This file was generated automatically during the make process
+// and it will be remade automatically
+#include<singular_axisym_navier_stokes/singular_axisym_fluid_traction_elements.h> 
+#include<singular_axisym_navier_stokes/singular_axisym_navier_stokes_elements.h> 
+#include<singular_axisym_navier_stokes/singular_axisym_navier_stokes.h> 
+#include<singular_axisym_navier_stokes/singular_axisym_sector_problem.h> 
