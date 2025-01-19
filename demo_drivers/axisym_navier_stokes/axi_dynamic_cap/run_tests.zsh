f00247baa08052a3d8cef5cc1b993e43af414cd0
Paul Alexander <paulalexander23@live.co.uk>
2025-01-19 17:43:05 +0000

    Log parallel jobs.

--- demo_drivers/axisym_navier_stokes/axi_dynamic_cap/run_tests.zsh
+++ demo_drivers/axisym_navier_stokes/axi_dynamic_cap/run_tests.zsh
@@ -1,2 +1,2 @@
 #!/usr/bin/zsh
-find TEST -name parameters.dat | parallel --results log ./axi_dynamic_cap --parameters {}
+find TEST -name parameters.dat | parallel --joblog tests.log --results log ./axi_dynamic_cap --parameters {}
