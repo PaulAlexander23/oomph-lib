d81e4f4c377d06585481d1eefb0e7abe78988814
Paul Alexander <paulalexander23@live.co.uk>
2025-02-03 15:34:29 +0000

    Comment out tests as they haven't been updated yet.

--- demo_drivers/linearised_free_surface_axisym_navier_stokes/axi_dynamic_cap_stability/run_tests.h
+++ demo_drivers/linearised_free_surface_axisym_navier_stokes/axi_dynamic_cap_stability/run_tests.h
@@ -1,28 +1,32 @@
 #ifndef RUN_TESTS_HEADER
 #define RUN_TESTS_HEADER
 
+// STD includes
 #include <iostream>
 
+// OOMPH-LIB include files
 #include "generic.h"
+#include "navier_stokes.h"
 #include "axisym_navier_stokes.h"
-#include "linearised_axisym_navier_stokes.h"
 #include "fluid_interface.h"
 #include "constitutive.h"
 #include "solid.h"
 #include "meshes/triangle_mesh.h"
+#include "linearised_axisym_navier_stokes.h"
 
-// Local include files
-#include "singular_axisym_dynamic_cap_problem.h"
-#include "perturbed_linear_stability_cap_problem.h"
+// Other demo includes
+#include "../../axisym_navier_stokes/axi_dynamic_cap/projectable_axisymmetric_Ttaylor_hood_elements.h"
+#include "../../axisym_navier_stokes/axi_dynamic_cap/singular_axisym_dynamic_cap_problem.h"
+#include "../../axisym_navier_stokes/axi_dynamic_cap/parameters.h"
+#include "../../axisym_navier_stokes/axi_dynamic_cap/utility_functions.h"
 
-#include "projectable_axisymmetric_Ttaylor_hood_elements.h"
-#include "singular_axisym_navier_stokes_elements.h"
+// Local includes
+#include "linearised_axisymmetric_fluid_interface_elements.h"
+#include "decomposed_linear_elasticity_elements.h"
+#include "linearised_elastic_axisym_fluid_interface_element.h"
+#include "overlaying_linearised_elastic_axisym_fluid_interface_element.h"
 #include "overlaying_my_linear_element.h"
-#include "overlaying_Tlinear_axisym_ns_pvd_elements.h"
-
-#include "parameters.h"
-
-#include "utility_functions.h"
+#include "perturbed_linear_stability_cap_problem.h"
 
 namespace oomph
 {
@@ -41,8 +45,7 @@ namespace oomph
 
   std::shared_ptr<AXISYM_PROBLEM> createBaseProblem();
   std::shared_ptr<PERTURBED_PROBLEM> createLinearProblem(
-    std::shared_ptr<AXISYM_PROBLEM> base_problem_pt,
-    const unsigned& azimuthal_mode_number = 0);
+    std::shared_ptr<AXISYM_PROBLEM> base_problem_pt, Params& parameters);
 
   enum
   {
