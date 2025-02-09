4acef6227692affc71ec3350232068b41fa8b8bf
Paul Alexander <paulalexander23@live.co.uk>
2025-02-09 14:50:54 +0000

    Tweaks in validate.sh and includeing validata to ensure tests pass on local machine.

--- demo_drivers/axisym_navier_stokes/axi_dynamic_cap/validate.sh
+++ demo_drivers/axisym_navier_stokes/axi_dynamic_cap/validate.sh
@@ -84,6 +84,10 @@ validate "continuation_run --arc --wall_velocity 0.01 --parameters" validata/obt
 validate "continuation_run --height_control --Bo 0.01 --parameters" validata/obtuse-unsteady-parameters-with-restart.dat trace.dat obtuse-height-cont-bo-results.dat.gz
 validate "continuation_run --height_control --wall_velocity 0.01 --parameters" validata/obtuse-unsteady-parameters-with-restart.dat trace.dat continuation_run_obtuse-height-continuation-parameters-with-restart_results.dat.gz
 
+touch RESLT
+rm -r -f RESLT
+mkdir RESLT
+
 var="./run_tests -t normal > Validation/OUTPUT"
 echo $var
 eval $var
