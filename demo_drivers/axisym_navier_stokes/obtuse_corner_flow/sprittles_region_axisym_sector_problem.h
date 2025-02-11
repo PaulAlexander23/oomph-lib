87d96c386eca3a5800bfa4dd5440aacb12179ab7
Paul Alexander <paulalexander23@live.co.uk>
2025-02-11 16:16:20 +0000

    Fix tests in navier stokes obtuse sector demo.

--- demo_drivers/axisym_navier_stokes/obtuse_corner_flow/sprittles_region_axisym_sector_problem.h
+++ demo_drivers/axisym_navier_stokes/obtuse_corner_flow/sprittles_region_axisym_sector_problem.h
@@ -63,8 +63,8 @@ namespace oomph
     };
 
     // Constructor
-    SprittlesRegionAxisymSectorProblem()
-      : RegionAxisymSectorProblem<ELEMENT>(), Contact_line_node_pt(0)
+    SprittlesRegionAxisymSectorProblem(Params& parameters)
+      : RegionAxisymSectorProblem<ELEMENT>(parameters), Contact_line_node_pt(0)
     {
       // Re-assign doc info pointer
       this->doc_info_pt()->set_directory("RESLT_axi_sprittles_region");
@@ -97,8 +97,6 @@ namespace oomph
         this->parameters().slip_length, Velocity_singular_function);
 
       Eigensolution_traction_function = eigensolution_traction_function_factory(
-        this->parameters().sector_angle * MathematicalConstants::Pi / 180.0,
-        Contact_line_node_pt,
         Grad_velocity_singular_function);
 
       create_singular_elements();
