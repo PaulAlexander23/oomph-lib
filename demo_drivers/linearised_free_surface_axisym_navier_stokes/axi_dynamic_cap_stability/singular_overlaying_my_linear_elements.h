af3875748f224603a539dd05ad7fbbf3b3c841d8
Paul Alexander <paulalexander23@live.co.uk>
2025-02-12 09:28:16 +0000

    Add a flag for fd jacobian. Add storage for solution elements.
    Add the generic residual functions.

--- demo_drivers/linearised_free_surface_axisym_navier_stokes/axi_dynamic_cap_stability/singular_overlaying_my_linear_elements.h
+++ demo_drivers/linearised_free_surface_axisym_navier_stokes/axi_dynamic_cap_stability/singular_overlaying_my_linear_elements.h
@@ -10,11 +10,22 @@ namespace oomph
     : public virtual OverlayingMyLinearElement<BASE_ELEMENT>
   {
   private:
+    /// Flag to indicate if the element has been augmented
     bool IsAugmented;
+
+    /// Flag to indicate if the Jacobian is computed using finite differences
+    bool IsJacobianFD;
+
+    /// Vector of pointers to SingularNavierStokesSolutionElement objects
+    Vector<
+      SingularNavierStokesSolutionElement<OverlayingMyLinearElement<BASE_ELEMENT>>*>
+      C_equation_elements_pt;
 
   public:
     SingularOverlayingMyLinearElement()
-      : OverlayingMyLinearElement<BASE_ELEMENT>(), IsAugmented(false)
+          : OverlayingMyLinearElement<BASE_ELEMENT>(),
+        IsAugmented(false),
+        IsJacobianFD(false)
     {
     }
 
@@ -23,9 +34,79 @@ namespace oomph
       return IsAugmented;
     }
 
+    // Check if the element is using finite differences for the Jacobian
+    bool is_using_fd_jacobian()
+    {
+      return IsJacobianFD;
+    }
+
     void augment()
     {
       IsAugmented = true;
+    }
+
+    void add_c_equation_element_pt(
+      SingularNavierStokesSolutionElement<
+        OverlayingMyLinearElement<BASE_ELEMENT>>* c_pt)
+    {
+      // Add the element
+      C_equation_elements_pt.push_back(c_pt);
+
+      // Add the additional unknown of this object as external data in the
+      // Navier-Stokes element
+      const bool use_fd = false;
+      this->add_external_data(c_pt->internal_data_pt(0), use_fd);
+    }
+
+    /// Add the element's contribution to its residual vector (wrapper)
+    void fill_in_contribution_to_residuals(Vector<double>& residuals)
+    {
+      // Call the generic residuals function with flag set to 0
+      // using a dummy matrix argument
+      // Get the contribution from the underlying wrapped element first
+      OverlayingMyLinearElement<
+        BASE_ELEMENT>::fill_in_contribution_to_residuals(residuals);
+
+      if (this->is_augmented())
+      {
+        this->fill_in_generic_residual_contribution_wrapped_axi_nst(
+          residuals, GeneralisedElement::Dummy_matrix, 0);
+      }
+    }
+
+    /// Add the element's contribution to its residual vector and
+    /// element Jacobian matrix (wrapper)
+    void fill_in_contribution_to_jacobian(Vector<double>& residuals,
+                                          DenseMatrix<double>& jacobian)
+    {
+      // Use finite differences
+      if (this->is_using_fd_jacobian())
+      {
+        FiniteElement::fill_in_contribution_to_jacobian(residuals, jacobian);
+      }
+      // Otherwise use analytic contributions
+      else
+      {
+        // Call the base fill_in_contribution_to_jacobian function
+        OverlayingMyLinearElement<BASE_ELEMENT>::fill_in_contribution_to_jacobian(
+          residuals, jacobian);
+        // Then call the singular Navier-Stokes element's
+        // fill_in_contribution_to_jacobian function
+        if (this->is_augmented())
+        {
+          this->fill_in_generic_residual_contribution_wrapped_axi_nst(
+            residuals, jacobian, 1);
+        }
+      }
+    }
+
+    /// Fill in the additional contributions to the momentum equations and
+    /// implement the total velocity equations
+    void fill_in_generic_residual_contribution_wrapped_axi_nst(
+      Vector<double>& residuals,
+      DenseMatrix<double>& jacobian,
+      const unsigned& flag)
+    {
     }
   };
 
