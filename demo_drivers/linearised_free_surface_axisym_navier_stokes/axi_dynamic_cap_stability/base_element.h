9fdf861d9b207902b4333aaf2643a68f05ca28aa
Paul Alexander <paulalexander23@live.co.uk>
2025-02-17 20:24:37 +0000

    Clean up includes.

--- demo_drivers/linearised_free_surface_axisym_navier_stokes/axi_dynamic_cap_stability/base_element.h
+++ demo_drivers/linearised_free_surface_axisym_navier_stokes/axi_dynamic_cap_stability/base_element.h
@@ -1,7 +1,7 @@
 #ifndef BASE_ELEMENT_HEADER
 #define BASE_ELEMENT_HEADER
 
-#include "axisym_navier_stokes.h"
+#include "axisym_navier_stokes/singular_axisym_navier_stokes_elements.h"
 #include "../../axisym_navier_stokes/axi_dynamic_cap/projectable_axisymmetric_Ttaylor_hood_elements.h"
 
 namespace oomph
