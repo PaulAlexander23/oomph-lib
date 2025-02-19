f58c0d631557441f355e3061d6aa275620d8a845
Paul Alexander <paulalexander23@live.co.uk>
2025-02-19 10:17:44 +0000

    Add a test case for mode 0 fixed c with a pinned outer wall velocity.

--- demo_drivers/linearised_free_surface_axisym_navier_stokes/axi_dynamic_cap_stability/run_tests.cc
+++ demo_drivers/linearised_free_surface_axisym_navier_stokes/axi_dynamic_cap_stability/run_tests.cc
@@ -121,6 +121,57 @@ BOOST_AUTO_TEST_CASE(show_artefact_mode_2)
   BOOST_TEST(abs(eigenvalue[0].real() - (-1.1117228111526063)) < 1e-6);
 }
 
+// Fix the singular scaling to test the internal and external boundary
+// conditions
+BOOST_AUTO_TEST_CASE(mode_0_fix_c)
+{
+  Params parameters;
+  //parameters.restart_filename = "RESLT/restart0.dat";
+  parameters.slip_length = 0.0;
+  parameters.contact_angle = 120.0 / 180.0 * MathematicalConstants::Pi;
+
+  // Create the base problem
+  BASE_PROBLEM base_problem(&parameters);
+  //  ifstream restart_filestream;
+  //  restart_filestream.open(parameters.restart_filename);
+  //  bool is_unsteady_restart = false;
+  //  base_problem.read(restart_filestream, is_unsteady_restart);
+  //  restart_filestream.close();
+  base_problem.steady_newton_solve();
+  base_problem.create_restart_file();
+
+  base_problem.reset_lagrange();
+  base_problem.assign_initial_values_impulsive();
+
+  // Create the linear problem
+  PERTURBED_PROBLEM perturbed_problem(base_problem.bulk_mesh_pt(),
+                                      base_problem.free_surface_mesh_pt(),
+                                      base_problem.slip_surface_mesh_pt(),
+                                      &parameters);
+
+  perturbed_problem.assign_initial_values_impulsive();
+  perturbed_problem.disable_singular_correction();
+
+  // Steady newton solve
+  perturbed_problem.steady_newton_solve();
+  perturbed_problem.doc_solution();
+
+  perturbed_problem.set_the_singular_correction(Vector<double>(2, 0.01));
+  perturbed_problem.pin_wall_velocity();
+  perturbed_problem.setup_new_data();
+  // perturbed_problem.set_boundary_conditions();
+  DoubleVector residuals;
+  perturbed_problem.get_residuals(residuals);
+  residuals.output("residuals.dat");
+  ofstream file("dofs.dat");
+  perturbed_problem.describe_dofs(file);
+  file.close();
+  perturbed_problem.doc_solution();
+
+  perturbed_problem.steady_newton_solve();
+  perturbed_problem.doc_solution();
+}
+
 
 // Mode 0
 // No displacement
@@ -131,32 +182,32 @@ BOOST_AUTO_TEST_CASE(show_artefact_mode_2)
 //   parameters.azimuthal_mode_number = 0;
 //   parameters.contact_angle = 120.0 / 180.0 * MathematicalConstants::Pi;
 //   //*parameters.wall_velocity_pt = 0.01;
-// 
+//
 //   // Create the base problem
 //   BASE_PROBLEM base_problem(&parameters);
 //   base_problem.steady_newton_solve();
 //   base_problem.reset_lagrange();
 //   base_problem.assign_initial_values_impulsive();
-// 
+//
 //   // Create the linear problem
 //   PERTURBED_PROBLEM perturbed_problem(base_problem.bulk_mesh_pt(),
 //                                       base_problem.free_surface_mesh_pt(),
 //                                       base_problem.slip_surface_mesh_pt(),
 //                                       &parameters);
-// 
+//
 //   perturbed_problem.pin_horizontal_mesh_deformation();
 //   perturbed_problem.pin_vertical_mesh_deformation();
 //   perturbed_problem.set_constant_lagrange_free_surface_boundary_condition(0.0,
 //                                                                           0.0);
 //   perturbed_problem.assign_initial_values_impulsive();
 //   perturbed_problem.doc_solution();
-// 
+//
 //   // Steady newton solve
 //   perturbed_problem.steady_newton_solve();
 //   perturbed_problem.doc_solution();
-// 
+//
 //   perturbed_problem.pin_horizontal_mesh_deformation();
-// 
+//
 //   // Eigensolve
 //   Vector<std::complex<double>> eigenvalue =
 //     perturbed_problem.solve_and_document_n_most_unstable_eigensolutions(1);
