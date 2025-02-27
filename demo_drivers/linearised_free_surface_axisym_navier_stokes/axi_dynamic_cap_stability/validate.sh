44ca498536e620ae948c5610ad9267295099fb52
Paul Alexander <paulalexander23@live.co.uk>
2025-02-27 14:29:13 +0000

    Remove now defunct run tests code, update golden master validata
    having determined the output to be correct.
    Update the Makefile and validate.sh script.

--- demo_drivers/linearised_free_surface_axisym_navier_stokes/axi_dynamic_cap_stability/validate.sh
+++ demo_drivers/linearised_free_surface_axisym_navier_stokes/axi_dynamic_cap_stability/validate.sh
@@ -5,7 +5,7 @@ OOMPH_ROOT_DIR=$(make -s --no-print-directory print-top_builddir)
 
 
 #Set the number of tests to be checked
-NUM_TESTS=9
+NUM_TESTS=8
 
 
 # Setup validation directory
@@ -67,27 +67,6 @@ validate "neutral_stability_run --Bo 0.01 --parameters" validata/parameters-with
 validate "neutral_stability_run --wall_velocity 0.01 --parameters" validata/parameters-with-restart.dat trace.dat
 #validate "neutral_stability_run --Bo 0.01 --parameters" validata/obtuse-parameters-with-restart0.dat trace.dat
 #validate "neutral_stability_run --wall_velocity 0.01 --parameters" validata/obtuse-parameters-with-restart.dat trace.dat
-
-mkdir RESLT -p 
-var="./run_tests > Validation/OUTPUT"
-echo $var
-eval $var
-echo "done"
-LOG="Validation/validation.log"
-echo " " >> $LOG 
-echo "Validation run" >> $LOG
-echo "---------------------------------------------" >> $LOG
-echo " " >> $LOG
-echo "Validation directory: " >> $LOG
-echo " " >> $LOG
-echo "  " `pwd` >> $LOG
-echo " " >> $LOG
-if grep "run_tests.cc" Validation/OUTPUT; then
-    cat Validation/OUTPUT >> $LOG
-    echo "[FAILED] -- Unit tests failed see validation log." >> $LOG
-else
-    echo "[OK] -- Unit tests passed" >> $LOG
-fi
 
 #######################################################################
 
