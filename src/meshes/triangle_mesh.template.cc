e9e70672f630a820abfdc55f5625857eab9ff945
Paul Alexander <paulalexander23@live.co.uk>
2025-04-17 13:58:06 +0100

    Extract class comaprison implementation from header file to its own cc
    file.

--- src/meshes/triangle_mesh.template.cc
+++ src/meshes/triangle_mesh.template.cc
@@ -33,6 +33,7 @@
 #include "../generic/multi_domain.h"
 #include "../generic/projection.h"
 #include "../generic/face_element_as_geometric_object.h"
+#include "classcomp.h"
 
 namespace oomph
 {
@@ -15196,99 +15197,6 @@ namespace oomph
   }
 
 #ifdef OOMPH_HAS_MPI
-
-  // ===================================================================
-  // The comparison class for the map that sorts the nodes on the
-  // shared boundary (using a lexicographic order)
-  // ===================================================================
-  struct classcomp
-  {
-    // Tolerance for lower-left comparison
-    static double Tol;
-
-
-    // Comparison operator for "lower left" ordering
-    bool operator()(const std::pair<double, double>& lhs,
-                    const std::pair<double, double>& rhs) const
-    {
-      double diff_y = lhs.second - rhs.second;
-      if (diff_y < -Tol) // (lhs.second < rhs.second)
-      {
-        return true;
-      }
-      else
-      {
-        // Are they "equal" with 1.0e-14 tolerance?
-        if (diff_y < Tol) // (lhs.second == rhs.second)
-        {
-#ifdef PARANOID
-          double diff_x = lhs.first - rhs.first;
-          if (fabs(diff_x) < Tol)
-          {
-            std::ostringstream warning_message;
-            warning_message
-              << "Dodgy \"lower left\" (lexicographic) comparison "
-              << "of points with cooordinates: "
-              << " lhs = ( " << lhs.first << " , " << lhs.second << " ) \n"
-              << " rhs = ( " << rhs.first << " , " << rhs.second << " ) \n"
-              << "x and y coordinates differ by less than tolerance!\n"
-              << "diff_x = " << diff_x << "\n"
-              << "diff_y = " << diff_y << "\n"
-              << "Tol    = " << Tol << "\n";
-            OomphLibError(warning_message.str(),
-                          OOMPH_CURRENT_FUNCTION,
-                          OOMPH_EXCEPTION_LOCATION);
-          }
-#endif
-          if (lhs.first < rhs.first)
-          {
-            return true;
-          }
-          else
-          {
-            return false;
-          }
-        }
-        else
-        {
-          return false;
-        }
-      }
-
-
-      // if (lhs.second < rhs.second)
-      //  {
-      //   return true;
-      //  }
-      // else
-      //  {
-      //   // // Are "equal" with 1.0e-14 tolerance
-      //   // if (lhs.second - rhs.second < 1.0e-14)
-      //   // Are equal?
-      //   if (lhs.second == rhs.second)
-      //    {
-      //     if (lhs.first < rhs.first)
-      //      {
-      //       return true;
-      //      }
-      //     else
-      //      {
-      //       return false;
-      //      }
-      //    }
-      //   else
-      //    {
-      //     return false;
-      //    }
-      //  }
-    }
-
-  } Bottom_left_sorter; // struct classcomp
-
-
-  // Assign value for tolerance
-  double classcomp::Tol = 1.0e-14;
-
 
   //======================================================================
   // Sort the nodes on shared boundaries so that the processors that share
